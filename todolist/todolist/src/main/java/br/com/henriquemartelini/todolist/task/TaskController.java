f1e19ead264aaa27f2faf8fb7d532d0635823840
Henrique Martelini <henriquezmartelini@hotmail.com>
2023-10-15 11:47:26 -0300

    Finalizacao

--- todolist/todolist/src/main/java/br/com/henriquemartelini/todolist/task/TaskController.java
+++ todolist/todolist/src/main/java/br/com/henriquemartelini/todolist/task/TaskController.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.Utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -26,26 +29,46 @@ public class TaskController {
   public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
     System.out.println("Chegou no controller");
     var idUser = request.getAttribute("idUser");
-    taskModel.setIdUser((UUID)idUser);
-    
+    taskModel.setIdUser((UUID) idUser);
+
     var currentDate = LocalDateTime.now();
-    if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-      .body("A data de inicio/termino da tarefa deve ser maior ou igual a atual!");
+          .body("A data de inicio/termino da tarefa deve ser maior ou igual a atual!");
     }
-    if(taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-      .body("A data de inicio dve ser menor do que a de termino!");
+          .body("A data de inicio dve ser menor do que a de termino!");
     }
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(HttpStatus.OK).body(task);
   }
-
 
   @GetMapping("/")
   public List<TaskModel> list(HttpServletRequest request) {
     var idUser = request.getAttribute("idUser");
     var tasks = this.taskRepository.findByIdUser((UUID) idUser);
     return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    if(task == null){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body("Tarefa nao encontrada");
+    }
+
+    var idUSer = request.getAttribute("idUSer");
+    if(task.getIdUser().equals(idUSer)){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body("Usuario nao tem permissao para alterar esta tarefa");
+    }
+    
+    Utils.copyNonNullProperties(taskModel, task);
+    var taskUpdated = this.taskRepository.save(task);
+    return ResponseEntity.ok().body(taskUpdated);
   }
 }
